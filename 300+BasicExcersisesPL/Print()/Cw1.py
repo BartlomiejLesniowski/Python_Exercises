e30434c2c217906c2608418b7ac6029c8fc47589
bart.lesniowski@gmail.com <bart.lesniowski@gmail.com>
2025-09-16 20:10:17 +0200

    Add file: Cw1.py Cw3.py

---
+++ 300+BasicExcersisesPL/Print()/Cw1.py
@@ -0,0 +1,6 @@
+"""Przypisz do zmiennej o nazwie message łańcuch znaków 'Learn Python!'.
+Następnie wydrukuj zmienną message do konsoli."""
+
+message = 'Learn Python!'
+
+print(message)
---
+++ 300+BasicExcersisesPL/Print()/Cw3.py
@@ -0,0 +1,16 @@
+"""Utwórz dwie zmienne, które będą przechowywać odpowiednio poniższe ciągi znaków:
+
+    'Python'
+    '3.10'
+
+
+Nazwy zmiennych możesz dobrać dowolnie. 
+Postaraj się jednak, aby były spójne z tym co dana zmienna przechowuje. 
+Korzystając z utworzonych zmiennych oraz funkcji print() wydrukuj do konsoli poniższy tekst.
+
+    Learning Python 3.10"""
+
+name = 'Python'
+version = '3.10'
+
+print(f'Learning {name} {version}')
