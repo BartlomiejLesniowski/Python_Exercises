ef57fef767aece1da167702124e9a55afecc3a18
BartlomiejLesniowski <bart.lesniowski@gmail.com>
2025-09-16 19:07:28 +0200

    Update README.md

---
+++ 300+BasicExcersisesPL/Cw1.py
@@ -0,0 +1,5 @@
+# Przypisz do zmiennej o nazwie message łańcuch znaków 'Learn Python!'. Następnie wydrukuj zmienną message do konsoli.
+
+message = 'Learn Python!'
+
+print(message)
---
+++ 300+BasicExcersisesPL/Print()/Cw2.py
@@ -0,0 +1,17 @@
+"""Ćwiczenie 2
+
+Wykonaj poniższe kroki:
+
+    przypisz do zmiennej age wartość 20
+
+    wykorzystując wartość zmiennej age wydrukuj do konsoli poniższą
+
+
+Oczekiwany wynik:
+
+
+    I am 20 years old."""
+
+age = 20
+
+print(f'I am {age} years old.')
